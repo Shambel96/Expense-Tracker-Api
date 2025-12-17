91342457fdde580eacebc9072eebbed5bc2078b0
Shambel Dechu <shambeldechu@gmail.com>
2025-12-17 23:35:55 +0300

    feat(user): add getAllUsers controller method

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -5,5 +5,10 @@ export const getAllUsers = async (req, res)=>{
         const conn = await db;
         const [rows]= await conn.query("SELECT * FROM users");
         res.json(rows);
-    }
+    } catch(err){
+        console.error("Error fetching users:", err);
+        res.status(500).json({
+            message: "Interval server error"
+        })
+}
 }
