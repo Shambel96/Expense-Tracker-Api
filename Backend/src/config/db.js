c9468fcc8b132106b56c2eb25b8285835c147dc6
Shambel Dechu <shambeldechu@gmail.com>
2026-01-29 16:01:18 +0300

    refactor(api): migrate from CommonJS to ES6

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,17 +1,21 @@
-const db = require("mysql2/promise");
-const dotenv = require("dotenv");
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-const connection = db.createConnection({
-    host: "localhost",
-    user: "expense_tracker",
-    password: "111111",
-    database: "expense_tracker",
-})
-connection.then(()=>{
-    console.log("Database connected successfully!");
-    
-}).catch((err)=>{
-    console.log("Database connection failed:", err);
-})
-module.exports = connection;
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "expense_tracker",
+    password: process.env.DB_PASSWORD || "111111",
+    database: process.env.DB_NAME || "expense_tracker",
+});
+
+connection
+    .then(() => {
+        console.log("Database connected successfully!");
+    })
+    .catch((err) => {
+        console.log("Database connection failed:", err);
+    });
+
+export default connection;
